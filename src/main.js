8e0cb986ffbfa11dc9212d14e180323160ed1af2
Darya-Zhulitova <darya7h@yandex.ru>
2025-04-12 01:51:26 +0300

    Initial commit

---
+++ src/main.js
@@ -0,0 +1,64 @@
+import './style.css'
+
+const API_KEY = 'live_BpmdIkOFGDwKN9gd42m67HqJOfSU3MyffAbhDD26YAZEUwLkTEiESd9GGLNGzsUQ';
+
+document.querySelector('#app').innerHTML = `
+    <header>
+        <h1>Cat Breeds</h1>
+    </header>
+    <div>
+        <section id="list">
+            <h2>Breeds</h2>
+            <ul id="list_ul"></ul>
+        </section>
+        <section id="item" class="hidden">
+            <h3 id="name"></h3>
+            <h4>Description:</h4>
+            <p id="description"></p>
+            <h4>Origin:</h4>
+            <p id="origin"></p>
+            <img src="" width="50%" alt="" decoding="async" id="image" loading="lazy">
+            <button id="close">Close</button>
+        </section>
+    </div>
+`
+
+
+document.querySelector('#close')?.addEventListener('click', () => {
+    document.querySelector('#list')?.classList.remove('hidden');
+    document.querySelector('#item')?.classList.add('hidden');
+});
+
+fetch('https://api.thecatapi.com/v1/breeds', {headers: {'x-api-key': API_KEY}})
+    .then(response => response.json())
+    .then(data => {
+        data.forEach(item => {
+            const li = document.createElement('li');
+            li.textContent = item.name;
+            li.addEventListener('click', () => {
+                showItem(item.id);
+            });
+            document.querySelector('#list_ul')?.appendChild(li);
+        });
+    });
+
+function showItem(id) {
+    fetch(`https://api.thecatapi.com/v1/breeds/${id}`, {headers: {'x-api-key': API_KEY}})
+        .then(response => response.json())
+        .then(data => {
+            document.querySelector('#name').textContent = data.name;
+            document.querySelector('#description').textContent = data.description;
+            document.querySelector('#origin').textContent = data.origin;
+            setRandomImage(id);
+            document.querySelector('#list').classList.add('hidden');
+            document.querySelector('#item').classList.remove('hidden');
+        });
+}
+
+function setRandomImage(id) {
+    fetch(`https://api.thecatapi.com/v1/images/search?limit=1&order=RAND&breed_id=${id}`, {headers: {'x-api-key': API_KEY}})
+        .then(response => response.json())
+        .then(data => {
+            document.querySelector('#image').src = data[0].url;
+        });
+}
